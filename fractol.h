e77570b1811991965c5f4fcd84e2b8ffb5e904cc
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-07-15 16:17:57 +0200

    fractol.h

---
+++ fractol.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 12:49:11 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/07/15 14:49:29 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "libft/libft.h"
+# include "mlx/mlx.h"
+
+# include <stdio.h>
+
+typedef struct s_data{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+//utils:
+int		ft_strcmp(char *s1, char *s2);
+void	window_init(void);
+void	avaliable_params(void);
+double	ft_atod(char *str);
+
+#endif
