9a9a3f50c9f0c45a5016d6adc220977866a23bce
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-07-16 17:45:38 +0200

    modified fractol.h

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:49:11 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/07/15 14:49:29 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:32:28 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,26 @@ typedef struct s_data{
 	int		endian;
 }				t_data;
 
+typedef struct s_vars{
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+typedef struct s_mlx{
+	void	*mlx;
+	void	*win;
+	t_data	img;
+	int		fractol;
+	double	x;
+	double	y;
+}				t_mlx;
+
 //utils:
 int		ft_strcmp(char *s1, char *s2);
-void	window_init(void);
+void	window_init(int fractol, double x, double y);
 void	avaliable_params(void);
 double	ft_atod(char *str);
+//int		close_win(int keycode, t_mlx *vars);
+void	hook_events(t_mlx *mlx);
 
 #endif
