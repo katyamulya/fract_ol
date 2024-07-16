de06d8a36de60337b95114f57b4bf73d3242ed78
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-07-16 17:45:22 +0200

    modified fractol.c

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 11:46:44 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/07/15 12:56:33 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:30:07 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ int	main(int argc, char **argv)
 	double	y;
 
 	if (argc == 2 && ft_strcmp(argv[1], "mandelbrot") == 0)
-		window_init();
+		window_init(1, 0.0, 0.0);
 	else if (argc == 4 && ft_strcmp(argv[1], "julia") == 0)
 	{
 		x = ft_atod(argv[2]);
-		//printf("%f\n", x);
 		y = ft_atod(argv[3]);
-		//printf("%f\n", y);
-		window_init();
+
+		window_init(2, x, y);
 	}
 	else
 		avaliable_params();
