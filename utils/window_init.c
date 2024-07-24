220368e20338c4c77ed09934f3ab990cc730360e
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-07-24 16:17:23 +0200

    modified window_init.c

--- utils/window_init.c
+++ utils/window_init.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:38:43 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/07/16 17:32:37 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/07/24 15:49:24 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,23 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)pix = color;
 }
 
-int	create_trgb(int t, int r, int g, int b)
-{
-	return (t << 24 | r << 16 | g << 8 | b);
-}
-
 void	init_var(t_mlx *mlx, int fractol, double x, double y)
 {
 	mlx->fractol = fractol;
 	mlx->x = x;
 	mlx->y = y;
+	mlx->zoom = 1;
+	mlx->movex = 0.0;
+	mlx->movey = 0.0;
+	mlx->max_i = 50;
+	mlx->color = 255;
 }
 
 void	window_init(int fractol, double x, double y)
 {
 	t_mlx	mlx;
-	int		col;
 
 	mlx.mlx = mlx_init();
-	init_var(&mlx, fractol, x, y);
 	if (fractol == 1)
 		mlx.win = mlx_new_window(mlx.mlx, 1000, 1000, "Mandelbrot");
 	else
@@ -46,47 +44,8 @@ void	window_init(int fractol, double x, double y)
 	mlx.img.img = mlx_new_image(mlx.mlx, 1000, 1000);
 	mlx.img.addr = mlx_get_data_addr(mlx.img.img, &mlx.img.bits_per_pixel,
 			&mlx.img.line_length, &mlx.img.endian);
-	col = create_trgb(0, 2, 100, 250);
-	my_mlx_pixel_put(&mlx.img, 50, 50, col);
-	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img.img, 0, 0);
+	init_var(&mlx, fractol, x, y);
 	hook_events(&mlx);
+	draw_fractol(&mlx);
 	mlx_loop(mlx.mlx);
 }
-
-/*
-void	window_init(void)
-{
-	t_mlx	mlx;
-
-	int y = 100;
-	int x;
-	int col;
-	int b = 255;
-
-	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, 1000, 1000, "Hello!");
-
-	mlx.img.img = mlx_new_image(mlx.mlx,  1000, 1000);
-	mlx.img.addr = mlx_get_data_addr(mlx.img.img, &mlx.img.bits_per_pixel, &mlx.img.line_length, &mlx.img.endian);
-
-	col = create_trgb(0, 2, 100, b);
-	while (y <= 350)
-	{
-		x = 100;
-		while (x <= 150)
-		{
-			my_mlx_pixel_put(&mlx.img, x, y, col);
-			mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img.img, 0, 0);
-			x++;
-		}
-		if (b == 0)
-			b = 255;
-		else
-			b--;
-		col = create_trgb(0, b / 10, b / 10, b);
-		y++;
-	}
-	hook_events(&mlx);
-	mlx_loop(mlx.mlx);
-}
-*/
